09fb7e37b723db1e92132cd34073537e0545e2ec
znerol <lo+github@znerol.ch>
2019-04-30 13:48:50 +0200

    chore(test): Setup more than one tls key/cert per server

--- molecule/default/tests/test_certbot.py
+++ molecule/default/tests/test_certbot.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
@@ -6,7 +7,26 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ["MOLECULE_INVENTORY_FILE"]).get_hosts("server")
 
 
-def test_certbot_issues_cert(host):
+@pytest.fixture
+def link_nginx_config():
+
+    created_links = []
+
+    def _link_nginx_config(host, cert_slug):
+        nginx_config_path = "/etc/certhub/{:s}.nginx.conf".format(cert_slug)
+        nginx_config_dest = "/etc/nginx/conf.d/{:s}.conf".format(cert_slug)
+        host.run_expect([0], "ln -s %s %s", nginx_config_path,
+                        nginx_config_dest)
+        created_links.append((host, cert_slug))
+
+    yield _link_nginx_config
+
+    for host, cert_slug in created_links:
+        nginx_config_dest = "/etc/nginx/conf.d/{:s}.conf".format(cert_slug)
+        host.run_expect([0], "rm -f %s", nginx_config_dest)
+
+
+def test_certbot_issues_cert(host, link_nginx_config):
     host_vars = host.ansible.get_variables()
     cert_slug = host_vars["inventory_hostname"]
     controller = host.get_host("ansible://{:s}?ansible_inventory={:s}".format(
@@ -14,9 +34,8 @@ def test_certbot_issues_cert(host):
         os.environ["MOLECULE_INVENTORY_FILE"]
     ))
 
-    # Ensure there are no failed units on server and controller
-    host.run_expect([0], "systemctl --quiet is-system-running")
-    controller.run_expect([0], "systemctl --quiet is-system-running")
+    # Install nginx config.
+    link_nginx_config(host, cert_slug)
 
     # Run certbot once on controller.
     service_name = "certhub-certbot-run@{:s}.service".format(cert_slug)
